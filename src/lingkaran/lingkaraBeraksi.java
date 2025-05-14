635f5829c876de6212493d4a18b77df97cfb2630
chelseaaaaapin <chelseariyantashila06@gmail.com>
2025-05-14 17:43:10 +0700

    Add files via upload

---
+++ src/lingkaran/lingkaraBeraksi.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lingkaran;
+
+/**
+ *
+ * @author Pandu
+ */
+public class lingkaraBeraksi {
+    public static void main(String[] args) {
+        lingkaran ling = new lingkaran();
+        
+        ling.hitungLuas(11.78);
+    }
+}
